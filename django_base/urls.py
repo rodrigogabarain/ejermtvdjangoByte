d9725529d31684b96be95b998c73bdc1b25a4b69
rodrigo gabarain <rodrigo.gabarain@gmail.com>
2023-03-06 11:28:52 -0300

    arreglos url y carga usuarios

--- django_base/urls.py
+++ django_base/urls.py
@@ -1,12 +1,16 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 from django.conf.urls.static import static
 from django.conf import settings
+
 
 from django_base.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' , index, name='index'),
+    path('', index, name='index'),
+
+    path('users/', include('users.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.db.models.signals import post_save
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.dispatch import receiver
 
-# Create your views here.
+from users.forms import RegisterForm
+from users.models import UserProfile
+
+def login_view(request):
+    if request.method == 'GET':
+        return render(request, 'users/login.html')
+    elif request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+        else:
+            context = {
+                'errors': form.errors,
+            }
+            return render(request, 'users/login.html', context)
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'users/register.html')
+    elif request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            context = {
+                'errors': form.errors,
+            }
+            return render(request, 'users/register.html', context)
+
+def users_list_view(request):
+    return render(request, 'users/users_list.html')
+
+def user_profile_view(request):
+    return render(request, 'users/user_profile.html')
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
